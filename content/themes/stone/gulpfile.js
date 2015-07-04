30acf3819dc3f97390c352c1fc494fa5a0346061
Steve Michelotti <steve.michelotti@gmail.com>
2015-07-04 08:45:29 -0400

    changed to explicit order for JS bundling

--- content/themes/stone/gulpfile.js
+++ content/themes/stone/gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var gulpLoadPlugins = require("gulp-load-plugins");
 var plugins = gulpLoadPlugins();
 
+var paths = {
+    vendorScripts: ['./lib/shCore.js', './lib/shBrushCSharp.js', './lib/shBrushJSharp.js', './lib/shBrushPowerShell.js', './lib/shBrushSql.js', './lib/shBrushVb.js', './lib/shBrushXml.js']    
+};
 
 gulp.task('bundlejs', function() {
   // return new queue({ objectMode: true })
@@ -13,7 +16,8 @@ gulp.task('bundlejs', function() {
     // .pipe(gulp.dest(paths.buildDir));
     
     
-    return gulp.src('./lib/*.js')
+    //return gulp.src('./lib/*.js')
+    return gulp.src(paths.vendorScripts)
     .pipe(plugins.uglify())
     .pipe(plugins.concat('lib_built.js'))
     //.pipe(gulp.dest('./dist/'));
