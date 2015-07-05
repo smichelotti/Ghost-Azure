d206e549e910a61697be27e47c34a84452166eef
Steve Michelotti <steve.michelotti@gmail.com>
2015-07-04 22:09:30 -0400

    add bundled css

--- content/themes/stone/gulpfile.js
+++ content/themes/stone/gulpfile.js
@@ -3,7 +3,8 @@ var gulpLoadPlugins = require("gulp-load-plugins");
 var plugins = gulpLoadPlugins();
 
 var paths = {
-    vendorScripts: ['./lib/shCore.js', './lib/shBrushCSharp.js', './lib/shBrushJScript.js', './lib/shBrushPowerShell.js', './lib/shBrushSql.js', './lib/shBrushVb.js', './lib/shBrushXml.js']    
+    vendorScripts: ['./lib/shCore.js', './lib/shBrushCSharp.js', './lib/shBrushJScript.js', './lib/shBrushPowerShell.js', './lib/shBrushSql.js', './lib/shBrushVb.js', './lib/shBrushXml.js'],
+    vendorCss: ['./lib/shThemeDefault.css', './lib/shCore.css']    
 };
 
 gulp.task('bundlejs', function() {
@@ -18,8 +19,15 @@ gulp.task('bundlejs', function() {
     
     //return gulp.src('./lib/*.js')
     return gulp.src(paths.vendorScripts)
-    .pipe(plugins.uglify())
+    .pipe(plugins.uglify({}))
     .pipe(plugins.concat('lib_built.js'))
     //.pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./lib/'));
+});
+
+gulp.task('bundlecss', function(){
+    return gulp.src(paths.vendorCss)
+    .pipe(plugins.minifyCss({}))
+    .pipe(plugins.concat('lib_built.css'))
     .pipe(gulp.dest('./lib/'));
 });
